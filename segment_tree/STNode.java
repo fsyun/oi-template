89814e5cee93240c778482de3e5acac31935f0ea
fsyun <feishenyun@gmail.com>
2023-02-11 12:56:50 -0800

    add dll & changed segment_tree

--- segment_tree/STNode.java
+++ segment_tree/STNode.java
@@ -1,78 +1,123 @@
 class STNode {
-    int from, to, mid;
-    long sumVal, incVal, mulVal;
+    long from, mid, to;
     STNode left, right;
-    STNode(int from, int to) {
+    long sumVal, minVal;
+    //parent marks have higher priority than children marks
+    boolean setFlag;
+    long setVal, incVal;
+    STNode(long from, long to) {
         this.from = from;
         this.to = to;
         mid = from + (to - from) / 2;
-        sumVal = 0;
-        incVal = 0;
-        mulVal = 1;
         left = null;
         right = null;
+        sumVal = 0;
+        minVal = 0;
+        setFlag = false;
     }
-    void inc(int p, int q, long val) {
-        sumVal = sumVal + val * (q - p + 1);
-        if (p == from && q == to) {
-            incVal = incVal + val;
-            return;
+    //for each modifying operation, it should be guarded by a pair of push and merge
+    //push to clear the marks for the current node
+    void push() {
+        if (from == to) {
+            if (setFlag) {
+                sumVal = setVal;
+                minVal = setVal;
+                setFlag = false;
+            }
+            if (incVal != 0) {
+                sumVal += incVal;
+                minVal += incVal;
+                incVal = 0;
+            }
+        } else {
+            if (left == null)
+                left = new STNode(from, mid);
+            if (right == null)
+                right = new STNode(mid + 1, to);
+            if (setFlag) {
+                left.setFlag = true;
+                left.setVal = setVal;
+                left.incVal = 0;
+                right.setFlag = true;
+                right.setVal = setVal;
+                right.incVal = 0;
+                setFlag = false;
+            }
+            if (incVal != 0) {
+                left.incVal += incVal;
+                right.incVal += incVal;
+                incVal = 0;
+            }
         }
-        if (p <= mid && left == null)
-            left = new STNode(from, mid);
-        if (q > mid && right == null)
-            right = new STNode(mid + 1, to);
-        if (q <= mid)
-            left.inc(p, q, val);
-        else if (p > mid)
-            right.inc(p, q, val);
+    }
+    //merge to keep data up-to-date
+    void merge() {
+        if (from < to) {
+            long leftLen = left.to - left.from + 1, rightLen = right.to - right.from + 1;
+            long leftSum = (left.setFlag ? left.setVal * leftLen : left.sumVal) + left.incVal * leftLen;
+            long rightSum = (right.setFlag ? right.setVal * rightLen : right.sumVal) + right.incVal * rightLen;
+            sumVal = leftSum + rightSum;
+            long leftMin = (left.setFlag ? left.setVal : left.minVal) + left.incVal;
+            long rightMin = (right.setFlag ? right.setVal : right.minVal) + right.incVal;
+            minVal = Math.min(leftMin, rightMin);
+        }
+    }
+    void set(long p, long q, long val) {
+        push();
+        if (from == p && to == q) {
+            setFlag = true;
+            setVal = val;
+            incVal = 0;
+        } else {
+            if (q <= mid)
+                left.set(p, q, val);
+            else if (p > mid)
+                right.set(p, q, val);
+            else {
+                left.set(p, mid, val);
+                right.set(mid + 1, q, val);
+            }
+        }
+        merge();
+    }
+    void inc(long p, long q, long val) {
+        push();
+        if (from == p && to == q)
+            incVal += val;
         else {
-            left.inc(p, mid, val);
-            right.inc(mid + 1, q, val);
+            if (q <= mid)
+                left.inc(p, q, val);
+            else if (p > mid)
+                right.inc(p, q, val);
+            else {
+                left.inc(p, mid, val);
+                right.inc(mid + 1, q, val);
+            }
         }
+        merge();
     }
-    void mul(int p, int q, long val) {
-        sumVal = sumVal * val;
-        if (p == from && q == to) {
-            mulVal = mulVal * val;
-            incVal = incVal * val;
-            return;
-        }
-        if (p <= mid && left == null)
-            left = new STNode(from, mid);
-        if (q > mid && right == null)
-            right = new STNode(mid + 1, to);
-        if (q <= mid)
-            left.mul(p, q, val);
-        else if (p > mid)
-            right.mul(p, q, val);
-        else {
-            left.mul(p, mid, val);
-            right.mul(mid + 1, q, val);
-        }
-    }
-    long sum(int p, int q) {
-        if (p == from && q == to)
+    long sum(long p, long q) {
+        push();
+        merge();
+        if (from == p && to == q)
             return sumVal;
-        pushDown();
-        if (q <= mid)
+        else if (q <= mid)
             return left.sum(p, q);
         else if (p > mid)
             return right.sum(p, q);
-        return left.sum(p, mid) + right.sum(mid + 1, q);
+        else
+            return left.sum(p, mid) + right.sum(mid + 1, q);
     }
-    void pushDown() {
-        if (left == null)
-            left = new STNode(from, mid);
-        if (right == null)
-            right = new STNode(mid + 1, to);
-        left.sumVal = left.sumVal * mulVal + (left.to - left.from + 1) * incVal;
-        right.sumVal = right.sumVal * mulVal + (right.to - right.from + 1) * incVal;
-        left.mulVal = left.mulVal * mulVal;
-        left.incVal = left.incVal * mulVal + incVal;
-        right.mulVal = right.mulVal * mulVal;
-        right.incVal = right.incVal * mulVal + incVal;
-        incVal = 0;
-        mulVal = 1;
+    long min(long p, long q) {
+        push();
+        merge();
+        if (from == p && to == q)
+            return minVal;
+        else if (q <= mid)
+            return left.min(p, q);
+        else if (p > mid)
+            return right.min(p, q);
+        else
+            return Math.min(left.min(p, mid), right.min(mid + 1, q));
     }
 }
